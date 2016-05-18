7e5eb7d13e86a95d1093f7660413587409250768
DetroitLabs <DetroitLabs@donovandlabsmacbookpro.local>
2016-05-17 21:39:54 -0400

    Initial Commit

---
+++ LightningRealm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LightningRealm
+//
+//  Created by DetroitLabs on 5/17/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LightningRealm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LightningRealm
+//
+//  Created by DetroitLabs on 5/17/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
